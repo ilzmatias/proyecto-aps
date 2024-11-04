d3d9ea3147ef3f774f06349dda932115b106e701
Romina Garcia <104693764+Romi212@users.noreply.github.com>
2024-11-04 18:54:55 -0300

    calendario citass

--- components/CalendarioDoctor.tsx
+++ components/CalendarioDoctor.tsx
@@ -1,13 +1,74 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
+import { supabase } from '../lib/supabaseClient';
 import 'react-calendar/dist/Calendar.css';
-
+import './CalendarioDoctor.module.css';
+interface Appointment {
+    legajo: string;
+    paciente_dni: string;
+    hora: string;
+    fecha: string;
+    // Agrega otros campos según sea necesario
+}
 const CalendarioDoctor = () => {
     const [date, setDate] = useState(new Date());
+    const [appointments, setAppointments] = useState<Appointment[]>([]);
+    const [datesWithAppointments, setDatesWithAppointments] = useState<string[]>([]);
 
-    const handleDateChange = (newDate) => {
+    useEffect(() => {
+        const fetchAppointmentsForMonth = async () => {
+            const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+            const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+            const formattedFirstDay = firstDayOfMonth.toISOString().split('T')[0];
+            const formattedLastDay = lastDayOfMonth.toISOString().split('T')[0];
+
+            const { data, error } = await supabase
+                .from('citas')
+                .select('*')
+                .gte('fecha', formattedFirstDay)
+                .lte('fecha', formattedLastDay)
+                .eq('medico_legajo', '12345'); // Reemplaza con el legajo del médico
+
+            if (error) {
+                console.error('Error fetching appointments:', error);
+                return;
+            }
+
+            //setAppointments(data as Appointment[]);
+            const dates = data.map((appointment: Appointment) => appointment.fecha);
+            setDatesWithAppointments(dates);
+        };
+        fetchAppointmentsForMonth();
+    }, [date]);
+    const handleDateChange = async (newDate) => {
         setDate(newDate);
+        const formattedDate = newDate.toISOString().split('T')[0]; // Formatear la fecha a YYYY-MM-DD
+        const data = await getAppointmentsByDate(formattedDate);
+        setAppointments(data);
+    };
+    const getAppointmentsByDate = async (date) => {
+        const { data, error } = await supabase
+            .from('citas')
+            .select('*')
+            .eq('fecha', date)
+            .eq('medico_legajo', '12345'); // Reemplaza con el legajo del médico
+
+        if (error) {
+            console.error('Error fetching appointments:', error);
+            return [];
+        }
+
+        return data as Appointment[];
+    };
+    const tileClassName = ({ date }) => {
+        
+            const formattedDate = date.toISOString().split('T')[0];
+            if (datesWithAppointments.includes(formattedDate)) {
+                return 'highlight';
+            }
+    
+        return null;
     };
 
     return (
@@ -19,13 +80,20 @@ const CalendarioDoctor = () => {
                     onChange={handleDateChange}
                     value={date}
                     className="bg-gray text-black rounded-lg p-2"
+                    tileClassName={tileClassName}
                 />
                 <div className="mt-4">
                     <h3 className="text-xl mb-2">Fecha seleccionada: {date.toDateString()}</h3>
                     <ul className="list-disc list-inside">
-                        <li>a</li>
-                        <li>b</li>
-                        <li>c</li>
+                    {appointments.length > 0 ? (
+                            appointments.map((appointment) => (
+                                <li>
+                                    <strong>DNI PACIENTE:</strong> {appointment.paciente_dni} <br />
+                                    <strong>Hora:</strong> {appointment.hora}
+                                </li>))
+                        ) : (
+                            <li>No hay citas para esta fecha</li>
+                        )}
                     </ul>
                 </div>
             </div>
